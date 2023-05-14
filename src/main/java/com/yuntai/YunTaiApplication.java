3eee063a341b2d530c6a115d4ec082a4c95f7ce6
humeng <humeng_mail@163.com>
2023-05-14 21:07:45 +0800

    init:新建启动类

---
+++ src/main/java/com/yuntai/YunTaiApplication.java
@@ -0,0 +1,11 @@
+package com.yuntai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunTaiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YunTaiApplication.class, args);
+    }
+}
